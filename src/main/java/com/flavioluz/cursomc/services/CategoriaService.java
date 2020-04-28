7a40fd7ac7c70716796fc6c670576017fba9cf5b
Flavio Luz <frpluz@gmail.com>
2020-04-28 18:54:55 -0400

    Listando todas categorias com DTO

--- src/main/java/com/flavioluz/cursomc/services/CategoriaService.java
+++ src/main/java/com/flavioluz/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.flavioluz.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,9 +41,11 @@ public class CategoriaService {
 		}
 		catch(DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma Categoria que possui Produtos");
-			
 		}
-		
 	}
+		
+		public List<Categoria> findAll(){
+			return repo.findAll();
+		}
 		
 }
