d3a73194cd7407d351e788c8b3b2b311042c0c4c
Flavio Luz <frpluz@gmail.com>
2020-05-01 18:09:53 -0400

    Inserindo Pedido

--- src/main/java/com/flavioluz/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/flavioluz/cursomc/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.flavioluz.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.flavioluz.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;	
 	
