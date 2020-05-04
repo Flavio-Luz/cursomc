3e671d037a91aaa8e9554db919285ee5b0f11e17
Flavio Luz <frpluz@gmail.com>
2020-05-04 16:15:42 -0400

    Configuracao e envio do projeto para producao

--- src/main/java/com/flavioluz/cursomc/domain/Cliente.java
+++ src/main/java/com/flavioluz/cursomc/domain/Cliente.java
@@ -28,7 +28,7 @@ public class Cliente implements Serializable {
 	private Integer id;
 	private String nome;
 	
-	@Column(unique=true)
+	@Column(unique=true, length = 190)
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipo;
